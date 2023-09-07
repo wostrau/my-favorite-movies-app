a527efcf86042677d65afc2f525e44da6b61e2f9
wostrau <a.ostrovtsov@gmail.com>
2023-09-07 17:33:30 +0200

    added functionality to collect data from inputs and store it in array of movies

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,10 @@ const addMovieModal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
 const startAddMovieButton = document.querySelector('header button');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.getElementsByTagName('input');
+
+const movies = [];
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
@@ -9,17 +13,61 @@ const toggleBackdrop = () => {
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+};
+
+const clearMovieInputs = () => {
+    for (const userInput of userInputs) {
+        userInput.value = '';
+    }
+};
+
+const startAddMovieButtonHandler = () => {
+    toggleMovieModal();
     toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
+    toggleBackdrop();
 };
 
 const backdropClickHandler = () => {
     toggleMovieModal();
+    toggleBackdrop();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+const confirmAddMovieButtonHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (
+        titleValue === '' ||
+        imageUrlValue === '' ||
+        ratingValue === '' ||
+        Number(ratingValue) < 1 ||
+        Number(ratingValue) > 5
+    ) {
+        alert('Please enter valid values! Rating should be a number between 1 and 5');
+        return;
+    }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    };
+
+    movies.push(newMovie);
+    console.log(movies);
+
+    clearMovieInputs();
+
+    toggleMovieModal();
+    toggleBackdrop();
+};
+
+startAddMovieButton.addEventListener('click', startAddMovieButtonHandler);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', confirmAddMovieButtonHandler);
